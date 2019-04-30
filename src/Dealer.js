ac2383a53e7587b9937e9460dd45bb470a01cb6c
Anthony DiRusso <ossurid@gmail.com>
2019-04-30 10:50:39 -0400

    creates starter files for Dealer & Player classes

---
+++ src/Dealer.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react'
+
+class Dealer extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+
+        }
+    }
+  render() {
+    return (
+      <div>
+        <h3>Hello, I'm the dealer.</h3>
+      </div>
+    )
+  }
+}
+
+
+export default Dealer;
---
+++ src/Player.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class Player extends Component {
+  render() {
+    return (
+      <div>
+        <h3>Hello, I'm the player.</h3>
+      </div>
+    )
+  }
+}
+
+export default Player;
